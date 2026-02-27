6d8c7cee5eaff2badb8517586906f3a226d0ea34
DeivyMateus <deivyandres30@gmail.com>
2026-02-27 06:46:28 -0500

    add folder ejercicios

---
+++ Ejercicios/EjerciciosPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejer6.Ejercicio6();
+        }
+    }
+}
