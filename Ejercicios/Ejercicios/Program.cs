637403d757aace2652652f310a113581629e223e
DeivyMateus <deivyandres30@gmail.com>
2026-02-26 22:19:56 -0500

    Promacion

---
+++ Ejercicios/Ejercicios/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejer1.Ejercicio1();
+            Ejer2.Ejercicio2();
+            Ejer3.Ejercicio3();
+            Ejer4.Ejercicio4();
+            Ejer5.Ejercicio5();
+            Ejer6.Ejercicio6();
+            Ejer7.Ejercicio7();
+            Ejer8.Ejercicio8();
+            Ejer9.Ejercicio9();
+            Ejer10.Ejercicio10();
+
+
+        }
+    }
+}
