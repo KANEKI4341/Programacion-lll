7459e65ded2c56a806b8db43931c0dbc45d34bdf
DeivyMateus <deivyandres30@gmail.com>
2026-03-19 15:06:03 -0500

    ejer 1

--- Reto1/Reto1/Program.cs
+++ Reto1/Reto1/Program.cs
@@ -11,18 +11,36 @@ namespace Reto1
     {
         static void Main(string[] args)
         {
-            string archivo = "cumpleaños.txt";
+            DateTime fechaActual = DateTime.Now;
+            string ruta = "cumpleaños.txt";
 
             for (int i = 0; i < 3; i++)
             {
                 Console.WriteLine("Ingrese el nombre del usuario: ");
                 string nombre = Console.ReadLine();
 
-                Console.WriteLine("Ingrese la fecha de nacimiento (dd/mm/aa): ");
-                Da
+                Console.WriteLine("Ingrese la fecha de nacimiento (aa/mm/dd): ");
+                string fecha = Console.ReadLine();
+                DateTime fechaNacimiento = DateTime.Parse(fecha);
 
+                DateTime cumpleaños = new DateTime(fechaActual.Year, fechaNacimiento.Month, fechaNacimiento.Day);
 
+                if (cumpleaños < fechaActual)
+                {
+                    cumpleaños = cumpleaños.AddYears(1);
+                }
+
+                TimeSpan falta = cumpleaños - fechaActual;
+                Console.WriteLine($"A {nombre} le faltan {falta.Days} días para su cumpleaños");
+
+                File.AppendAllText(ruta, $"{nombre} - {fechaNacimiento.ToShortDateString()}{Environment.NewLine}");
             }
+
+            string contenido = File.ReadAllText(ruta);
+            Console.WriteLine("\nContenido del archivo:");
+            Console.WriteLine(contenido);
         }
     }
 }
+
+
